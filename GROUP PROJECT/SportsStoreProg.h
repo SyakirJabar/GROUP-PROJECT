004fcb22cded30f5d4c82894606970b3a740de0c
phoenix-d0wn <182614029+phoenix-d0wn@users.noreply.github.com>
2026-01-04 11:34:29 +0800

    Added SportsStoreProg declaration

--- GROUP PROJECT/SportsStoreProg.h
+++ GROUP PROJECT/SportsStoreProg.h
@@ -1,8 +1,14 @@
-#include<iostream>
-#include<fstream>
-#include<iomanip>
-#include<cstring>
-//This is our group project
-int main() {
+//SportsStoreProg.h
 
-}
+class SportsStoreProg
+{
+private:
+    std::vector<std::unique_ptr<Equipment>> equipments;  //sports equipments stored
+
+public:
+    void insEquip();
+    void displayAllEquip();
+    void searchEquip();
+    void printSummary();
+    void menu();
+};
