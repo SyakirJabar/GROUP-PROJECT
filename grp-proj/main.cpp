b76cbf6314c088af0190266498a9f4d51ddcf5be
phoenix-d0wn <182614029+phoenix-d0wn@users.noreply.github.com>
2026-01-04 13:23:16 +0800

    Created main.cpp

---
+++ grp-proj/main.cpp
@@ -0,0 +1,13 @@
+//main.cpp
+
+#include "SportsEquip/Ball.h"
+#include "SportsEquip/Racket.h"
+#include "SportsEquip/Projectile.h"
+#include "SportsStoreProg/SportsStoreProg.h"
+
+int main()
+{
+    SportsStoreProg sportsStoreProg;
+
+    return 0;
+}
