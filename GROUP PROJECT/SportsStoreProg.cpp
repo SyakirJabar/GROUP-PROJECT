5842e3ef0980d47a420de9e8e2cfe358c281eab9
phoenix-d0wn <182614029+phoenix-d0wn@users.noreply.github.com>
2026-01-04 11:55:32 +0800

    Deleted MenuController.h and added it as the constructor of the SportsStoreProg struct

--- GROUP PROJECT/SportsStoreProg.cpp
+++ GROUP PROJECT/SportsStoreProg.cpp
@@ -11,7 +11,42 @@ typedef std::string str;
 
 //constructor
 SportsStoreProg::SportsStoreProg()
-{}
+{
+    char usrIn{};
+    bool exit{};
+
+    std::cout << "Welcome\n";
+    std::cout << "-------\n";
+
+    while (true)
+    {
+        
+        std::cout << "1. Insert Record\n";
+        std::cout << "2. Display All Records\n";
+        std::cout << "3. Search Records\n";
+        std::cout << "4. Print Report\n";
+        std::cout << "q to exit\n";
+        std::cout << "-----------------------\n";
+        std::cout << "User input: ";
+        
+        std::cin >> usrIn;
+
+        switch (usrIn)
+        {
+        case '1': insEquip();
+                  break;
+        case '2': dispAllEquip();
+                  break;
+        case '3': searchEquip();
+                  break;
+        case '4': printSummary();
+                  break;
+        case 'q': return;
+                  break;
+        default : std::cout << "Invalid input. Try again...\n\n";
+        }
+    }
+}
 
 //destructor
 SportsStoreProg::~SportsStoreProg()
