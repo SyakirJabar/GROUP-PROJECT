1e69aa8abba36472fcf7f09203588bf1bb41a850
phoenix-d0wn <182614029+phoenix-d0wn@users.noreply.github.com>
2026-01-04 12:20:10 +0800

    Deleted all setters from SportsEquip

--- GROUP PROJECT/SportsEquip/SportsEquip.h
+++ GROUP PROJECT/SportsEquip/SportsEquip.h
@@ -11,18 +11,11 @@ class SportsEquip
 public:
     ~SportsEquip();     //destructor
 
-    //setters (virtual)
-    virtual void setName(const str&) = 0;
-    virtual void setCateg(const str&) = 0;  //category of sports equipment
-    virtual void setGoodQ(const int&) = 0;  //quantity of equipments not broken or lost
-    virtual void setBrokenQ(const int&) = 0;
-    virtual void setLostQ(const int&) = 0;
-
     //getters (virtual)
     //does not modify object representation
     virtual str getName() const = 0;
-    virtual str setCateg() const = 0;
-    virtual int setGoodQ() const = 0;  
+    virtual str setCateg() const = 0;        //category of sports equipment
+    virtual int setGoodQ() const = 0;        //quantity of equipments not broken or lost
     virtual int setBrokenQ() const = 0;
     virtual int setLostQ() const = 0;
 };  
