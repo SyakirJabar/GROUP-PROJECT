ae76cb0d76534b412f9369388bea406847b82aab
phoenix-d0wn <182614029+phoenix-d0wn@users.noreply.github.com>
2026-01-06 01:23:59 +0800

    Changed 2D vector name

--- grp-proj-v3/SportsStoreProg/SportsStoreProg.h
+++ grp-proj-v3/SportsStoreProg/SportsStoreProg.h
@@ -16,7 +16,7 @@ private:
     //vector storing (unique) pointers to objects of classes derived 
     //from SportsEquip (base) class
     std::vector<std::unique_ptr<SportsEquip>> eqpmnts;  //sports equipments
-    std::vector<std::vector<str>> eqpmnts2D;
+    std::vector<std::vector<str>> eqpmntsVal;
 
 public:
     SportsStoreProg();      //constructor
